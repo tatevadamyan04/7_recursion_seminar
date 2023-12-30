e9e332c4e679ecc30113c83ca5bc7605af4f9b79
Tatevik Adamyan <tatevadamyan2004@gmail.com>
2023-12-30 23:08:48 +0400

    Task 3 is done

---
+++ Task 3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+Console.WriteLine("Original Array:");
+PrintArray(array);
+
+Console.WriteLine("\nArray in Reverse Order:");
+PrintArrayReverse(array, array.Length - 1);
+
+static void PrintArrayReverse(int[] array, int index)
+{
+    if (index >= 0)
+    {
+        Console.Write($"{array[index]} ");
+        PrintArrayReverse(array, index - 1);
+    }
+}
+
+static void PrintArray(int[] array)
+{
+    foreach (var element in array)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
